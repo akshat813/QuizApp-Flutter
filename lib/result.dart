8f31e9c4d6f9cdd9a775b6a5c567fa06573e2c34
akshat813 <akshat.gupta813@gmail.com>
2020-04-25 21:26:46 +0530

    Flutter Quiz App

---
+++ lib/result.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome!';
+    } else if (resultScore <= 12) {
+      resultText = 'Impressive!';
+    } else if (resultScore <= 16) {
+      resultText = 'Nice Taste?!';
+    } else {
+      resultText = 'You are  bad at this !';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text(
+              'Restart Quiz!',
+            ),
+            textColor: Colors.red,
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
+    );
+  }
+}
